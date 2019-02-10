745f0f2d4d6a87af5201dc223d03bdf2a0bdd568
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2019-02-10 12:37:28 -0400

    Primer commit

---
+++ src/logica/AplicacionRestaurante.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+/**
+ *
+ * @authores:
+    * Anthony Chacin
+    * José Cisneros
+ */
+public class AplicacionRestaurante {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
